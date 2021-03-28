91005ca5fc493a51a6493e44f2b82b111a92b4d1
Dzjnt <kosqer@wp.pl>
2021-03-28 17:17:37 +0200

    Added connection string to appsettings and configurate dependency injection

--- FitApp/Startup.cs
+++ FitApp/Startup.cs
@@ -1,7 +1,9 @@
+using FitApp.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,9 @@ namespace FitApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+            services.AddDbContext<FitStorageContext>(
+                opt => opt.UseSqlServer(this.Configuration.GetConnectionString("FitStorageDatabaseConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
