0d057fa0243d23872cb306c8e27c926bdb276841
Dzjnt <kosqer@wp.pl>
2021-03-30 07:37:23 +0200

    Added Application services class library and MediatR

---
+++ FitApp.ApplicationServices/API/Domain/GetRecipesRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitApp.ApplicationServices.API.Domain
+{
+    public class GetRecipesRequest : IRequest<GetRecipesResponse>
+    {
+
+    }
+}
---
+++ FitApp/Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+﻿using FitApp.DataAccess;
+using FitApp.DataAccess.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRepository<Recipe> _recipeRepository;
+        public RecipeController(IRepository<Recipe> recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Recipe> GetRecipes() => _recipeRepository.GetAll();
+
+        [HttpGet]
+        [Route("{id}")]
+        public Recipe GetRecipe(int id) => _recipeRepository.GetById(id);
+
+
+    }
+}
